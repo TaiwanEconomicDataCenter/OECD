8826ad20ffdb05b876b8b16ecc7da0c659e7f962
TaiwanEconomicDataCenter <tedc1@ms12.hinet.net>
2022-02-23 18:03:43 +0800

    Update Quarterly.java

--- OECD_Lib/src/tedc/oecd/entity/Quarterly.java
+++ OECD_Lib/src/tedc/oecd/entity/Quarterly.java
@@ -1,5 +1,8 @@
 package tedc.oecd.entity;
 
+import java.time.Year;
+import java.time.format.DateTimeParseException;
+
 import org.threeten.extra.YearQuarter;
 
 public class Quarterly extends TimeRange {
@@ -9,24 +12,43 @@ public class Quarterly extends TimeRange {
 	public void setStartTime(YearQuarter startTime) {
 		this.startTime = startTime;
 	}
-	public void setStartTime(YearQuarter startTime) {
-		this.startTime = startTime;
+	public void setStartTime(String startTime) {
+		if(checkQuarter(startTime)) {
+			this.startTime = YearQuarter.parse(startTime);
+		}else {
+			throw new RuntimeException("季資料格式不正確!");
+		}
 	}
 
 	public void setEndTime(YearQuarter endTime) {
 		this.endTime = endTime;
 	}
+	public void setEndTime(String endTime) {
+		if(checkQuarter(endTime)) {
+			this.endTime = YearQuarter.parse(endTime);
+		}else {
+			throw new RuntimeException("季資料格式不正確!");
+		}
+	}
+	
+	public boolean checkQuarter(String time) {
+		try {
+			YearQuarter yq = YearQuarter.parse(time);
+		}catch(DateTimeParseException e) {
+			return false;
+		}
+		
+		return true;
+	}
 
 	@Override
 	public String getStartTime() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.startTime.toString();
 	}
 
 	@Override
 	public String getEndTime() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.endTime.toString();
 	}
 
 }
